d9fbb681a71be99bfd97e51331f1d71e2a532e00
datnguyen.bio <65652962+datnguyen-bio@users.noreply.github.com>
2024-08-12 15:28:43 +0700

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import streamlit as st
+import pandas as pd
+import subprocess
 
 st.title("Trung tam tin hoc")
 st.subheader("How to run streamlit app")
 
-menu = ["Home", "About"]
+menu = ["Home", "About", "Hotel Comments", "Hotel Comments Recommendation"]
 choice = st.sidebar.selectbox('Menu', menu)
+
 if choice == 'Home':
     st.subheader('Streamlit From Windows')
 elif choice == 'About':
     st.subheader("[Trung tam tin hoc](https://csc.edu.vn)")
+elif choice == 'Hotel Comments':
+    st.subheader("hotel_comments")
+    hotel_comments = pd.read_csv("hotel_comment.csv")
+    st.write(hotel_comments)
+elif choice == 'Hotel Comments Recommendation':
+    st.subheader("hotel_comments_recommendation")
+    # Run the 'cosin_gensim.ipynb' notebook
+    subprocess.call(["jupyter", "notebook", "cosin_gensim.ipynb"])
