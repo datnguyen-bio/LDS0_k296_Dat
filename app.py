8d9b09cc04d0eddb0d2444a0ed6858f7b065d67a
datnguyen.bio <65652962+datnguyen-bio@users.noreply.github.com>
2024-08-23 11:32:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -212,13 +212,12 @@ elif choice == '2. Phân tích dữ liệu':
         # Display the word cloud
         text = ' '.join(selected_hotel['Processed_Body'])
         wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
-    
         # Display the word cloud
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.imshow(wordcloud, interpolation='bilinear')
         ax.set_axis_off()
         st.pyplot(fig)
-    else:
+            else:
         st.write("Chưa chọn nơi lưu trú cần phân tích.")
 
 elif choice == '3. Sentiment Analysis':
